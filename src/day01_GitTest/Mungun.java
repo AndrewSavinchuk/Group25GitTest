9689034c9775af56e403a7b7ad0721d9a7c51c69
Munguntsatsral <tsatsaa5mn@gmail.com>
2022-01-24 15:07:50 -0600

    tc001 new branch is created

--- src/day01_GitTest/Mungun.java
+++ src/day01_GitTest/Mungun.java
@@ -5,5 +5,7 @@ public class Mungun {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("No more trouble )))");
+
+        // step 1
     }
 }
