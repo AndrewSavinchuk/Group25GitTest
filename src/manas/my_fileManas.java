074fc1d0e146b44aa5057fe77bdbceb95c31ddaf
Max Manas Kalenov <manasworks@gmail.com>
2022-01-24 15:31:08 -0600

    Test code

--- src/manas/my_fileManas.java
+++ src/manas/my_fileManas.java
@@ -2,6 +2,6 @@ package manas;
 
 public class my_fileManas {
     public static void main(String[] args) {
-        System.out.println("Mabas");
+        System.out.println("HHHHaaa HO HOH");
     }
 }
